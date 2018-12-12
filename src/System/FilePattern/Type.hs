7684c84f6555677ab06cebbcb5f1f528a9c6418f
Neil Mitchell <ndmitchell@gmail.com>
2018-12-12 21:00:41 +0000

    Add new parsers for paths and patterns

--- src/System/FilePattern/Type.hs
+++ src/System/FilePattern/Type.hs
@@ -2,6 +2,8 @@
 -- | The type of patterns and wildcards
 module System.FilePattern.Type(
     FilePattern,
+    Pattern(..), parsePattern,
+    Path(..), parsePath,
     Pats, fromPats, mkPats,
     Pat(..),
     toWildcard,
@@ -10,6 +12,7 @@ module System.FilePattern.Type(
     ) where
 
 import System.FilePattern.Wildcard
+import System.FilePath
 import Data.List.Extra
 
 
@@ -25,18 +28,6 @@ toWildcard (Pats xs) = case map (map unstars) $ split (== Skip) xs of
         Just (mid, post) -> Wildcard pre mid post
     where unstars (Stars x) = x
           unstars Skip = error "toWildcard: impossible - already split on Skip"
-
-
-
--- | A type synonym for file patterns, containing @**@ and @*@. For the syntax
---   and semantics of 'FilePattern' see '?=='.
---
---   Most 'FilePath' values lacking literal @.@ and @..@ components are suitable as 'FilePattern' values which match
---   only that specific file. On (Windows @\\@ is treated as equivalent to @\/@.
---
---   You can write 'FilePattern' values as a literal string, or build them
---   up using the operators '<.>' and '</>'.
-type FilePattern = String
 
 
 -- | Parsed 'FilePattern'.
@@ -60,3 +51,37 @@ lit = Stars . Literal
 fromLit :: Pat -> Maybe String
 fromLit (Stars (Literal x)) = Just x
 fromLit _ = Nothing
+
+
+
+-- | A type synonym for file patterns, containing @**@ and @*@. For the syntax
+--   and semantics of 'FilePattern' see '?=='.
+--
+--   Most 'FilePath' values lacking literal @.@ and @..@ components are suitable as 'FilePattern' values which match
+--   only that specific file. On (Windows @\\@ is treated as equivalent to @\/@.
+--
+--   You can write 'FilePattern' values as a literal string, or build them
+--   up using the operators '<.>' and '</>'.
+type FilePattern = String
+
+
+newtype Path = Path [String]
+
+newtype Pattern = Pattern (Wildcard [Wildcard String])
+
+
+split0 :: (a -> Bool) -> [a] -> [[a]]
+split0 f [] = []
+split0 f xs = split f xs
+
+parsePath :: FilePath -> Path
+parsePath = Path . split0 isPathSeparator
+
+parsePattern :: FilePattern -> Pattern
+parsePattern = Pattern . fmap (map $ f '*') . f "**" . split0 isPathSeparator
+    where
+        f :: Eq a => a -> [a] -> Wildcard [a]
+        f x xs = case split (== x) xs of
+            pre:mid_post -> case unsnoc mid_post of
+                Nothing -> Literal pre
+                Just (mid, post) -> Wildcard pre mid post

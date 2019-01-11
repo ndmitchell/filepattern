5ca4c76e0cb20ff63ab98844d30cd987e8de5120
Neil Mitchell <ndmitchell@gmail.com>
2019-01-11 11:50:17 +0000

    Move the test cases into a separate file

--- test/Test.hs
+++ test/Test.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 
 module Main(main) where
 
@@ -6,11 +7,10 @@ import Data.List.Extra
 import qualified Test.Util as T
 import Data.Maybe
 import System.FilePattern as FilePattern
-import System.FilePath(isPathSeparator, (</>))
+import System.FilePath(isPathSeparator)
 import System.IO.Unsafe
-import System.Info.Extra
 import Test.QuickCheck
-
+import Test.Cases
 
 ---------------------------------------------------------------------
 -- TEST UTILITIES
@@ -64,168 +64,14 @@ showWalk (WalkTo (p,xs)) = "WalkTo " ++ pair (show p) (list [pair (show a) (show
 
 main :: IO ()
 main = do
-    let dot x = putStr "." >> x
-    putStr "Testing "
-    dot testSimple
-    dot testArity
-    dot testSubstitute
-    dot testMatch
+    putStr "Testing..."
+    testCases
     T.TestData{..} <- T.unsafeTestData
     putStrLn $ "Specific test cases passed (" ++ show testDataCases ++ ")"
     -- when False $ dot $ testWalk s
     putStr " "
     testProperties []
 
-
-testSimple :: IO ()
-testSimple = do
-    T.simple "a*b" False
-    T.simple "a//b" True
-    T.simple "a/**/b" False
-    T.simple "/a/b/cccc_" True
-    T.simple "a///b" True
-    T.simple "a/**/b" False
-
-
-testArity :: IO ()
-testArity = do
-    T.arity "" 0
-    T.arity "foo/**/*" 2
-    T.arity "//*a.txt" 1
-    T.arity "foo//a*.txt" 1
-    T.arity "**/*a.txt" 2
-    T.arity "foo/**/a*.txt" 2
-    T.arity "//*a.txt" 1
-    T.arity "foo//a*.*txt" 2
-    T.arity "foo/**/a*.*txt" 3
-
-
-testSubstitute :: IO ()
-testSubstitute = do
-    T.substitute "**/*a*.txt" ["","test","da"] "testada.txt"
-    T.substitute "**/*a.txt" ["foo/bar","test"] "foo/bar/testa.txt"
-    -- error if the number of replacements is wrong
-    T.substituteErr "nothing" ["test"] ["substitute","nothing","expects 0","got 1","test"]
-    T.substituteErr "*/*" ["test"] ["substitute","*/*","expects 2","got 1","test"]
-
-
-testMatch :: IO ()
-testMatch = do
-    T.matchN "//*.c" "foo/bar/baz.c"
-    --T.matchY "//*.c" "/baz.c" ["baz"]
-    T.matchY "**/*.c" "foo/bar/baz.c" ["foo/bar","baz"]
-    T.matchY ("**" </> "*.c") ("foo/bar" </> "baz.c") ["foo/bar","baz"]
-    T.matchY "*.c" "baz.c" ["baz"]
-    T.matchN "//*.c" "baz.c"
-    T.matchY "**/*.c" "baz.c" ["","baz"]
-    T.matchY "**/*a.txt" "foo/bar/testa.txt" ["foo/bar","test"]
-    T.matchN "**/*.c" "baz.txt"
-    T.matchY "**/*a.txt" "testa.txt" ["","test"]
-    T.matchY "**/a.txt" "a.txt" [""]
-    T.matchY "a/**/b" "a/b" [""]
-    T.matchY "a/**/b" "a/x/b" ["x"]
-    T.matchY "a/**/b" "a/x/y/b" ["x/y"]
-    T.matchY "a/**/**/b" "a/x/y/b" ["","x/y"]
-    T.matchY "**/*a*.txt" "testada.txt" ["","test","da"]
-    T.matchY "test.c" "test.c" []
-    T.matchN "*.c" "foor/bar.c"
-    T.matchN "*/*.c" "foo/bar/baz.c"
-    T.matchN "foo//bar" "foobar"
-    T.matchN "foo/**/bar" "foobar"
-    T.matchN "foo//bar" "foobar/bar"
-    T.matchN "foo/**/bar" "foobar/bar"
-    T.matchN "foo//bar" "foo/foobar"
-    T.matchN "foo/**/bar" "foo/foobar"
-    -- T.matchY "foo//bar" "foo/bar" []
-    T.matchY "foo/**/bar" "foo/bar" [""]
-    T.matchY "foo/bar" ("foo" </> "bar") []
-    T.matchY ("foo" </> "bar") "foo/bar" []
-    T.matchY ("foo" </> "bar") ("foo" </> "bar") []
-    T.matchY "**/*.c" ("bar" </> "baz" </> "foo.c") ["bar/baz","foo"]
-    -- T.matchY "//*" "/bar" ["bar"]
-    T.matchY "**/*" "/bar" ["/","bar"]
-    T.matchN "/bob//foo" "/bob/this/test/foo"
-    -- T.matchY "/bob//foo" "/bob/foo" []
-    T.matchY "/bob/**/foo" "/bob/this/test/foo" ["this/test"]
-    T.matchN "/bob//foo" "bob/this/test/foo"
-    T.matchN "/bob/**/foo" "bob/this/test/foo"
-    T.matchN "bob//foo/" "bob/this/test/foo/"
-    -- T.matchY "bob//foo/" "bob/foo/" []
-    T.matchY "bob/**/foo/" "bob/this/test/foo/" ["this/test"]
-    T.matchY "bob/**/foo/" "bob/foo/" [""]
-    T.matchY "bob/**/foo/" "bob//foo/" ["/"]
-    T.matchN "bob//foo/" "bob/this/test/foo"
-    T.matchN "bob/**/foo/" "bob/this/test/foo"
-    T.matchY ("**" </> "*a*.txt") "testada.txt" ["","test","da"]
-    T.matchN "a//" "a"
-    T.matchY "a/**" "a" [""]
-    -- T.matchY "a//" "a/" []
-    T.matchN "/a//" "/a"
-    T.matchY "a/**" "a" [""]
-    -- T.matchY "/a//" "/a/" []
-    T.matchY "/a/**" "/a" [""]
-    T.matchN "///a//" "/a"
-    -- T.matchY "///a//" "/a/" []
-    T.matchY "**/a/**" "/a" ["/",""]
-    T.matchN "///" ""
-    -- T.matchY "///" "/" []
-    T.matchY "/**" "/" ["/"]
-    T.matchY "**/" "a/" ["a"]
-    -- T.matchY "////" "/" []
-    -- T.matchY "**/**" "" ["","/"]
-    T.matchY "x/**/y" "x/y" [""]
-    -- T.matchY "x///" "x/" []
-    T.matchY "x/**/" "x/" [""]
-    T.matchY "x/**/" "x/foo/" ["foo"]
-    T.matchN "x///" "x"
-    T.matchN "x/**/" "x"
-    T.matchY "x/**/" "x/foo/bar/" ["foo/bar"]
-    T.matchN "x///" "x/foo/bar"
-    T.matchN "x/**/" "x/foo/bar"
-    -- T.matchY "x///y" "x/y" []
-    T.matchY "x/**/*/y" "x/z/y" ["","z"]
-    T.matchY "" "" []
-    T.matchN "" "y"
-    T.matchN "" "/"
-
-    T.matchY "*/*" "x/y" ["x","y"]
-    T.matchN "*/*" "x"
-    -- T.matchY "//*" "/x" ["x"]
-    T.matchY "**/*" "x" ["","x"]
-    -- T.matchY "//*" "/" [""]
-    -- T.matchY "**/*" "" ["",""]
-    -- T.matchY "*//" "x/" ["x"]
-    T.matchY "*/**" "x" ["x",""]
-    -- T.matchY "*//" "/" [""]
-    -- T.matchY "*//*" "x/y" ["x","y"]
-    T.matchY "*/**/*" "x/y" ["x","","y"]
-    T.matchN "*//*" ""
-    T.matchN "*/**/*" ""
-    T.matchN "*//*" "x"
-    T.matchN "*/**/*" "x"
-    T.matchN "*//*//*" "x/y"
-    T.matchN "*/**/*/**/*" "x/y"
-    -- T.matchY "//*/" "//" [""]
-    T.matchY "**/*/" "/" ["",""]
-    -- T.matchY "*/////" "/" [""]
-    T.matchY "*/**/**/" "/" ["","",""]
-    T.matchN "b*b*b*//" "bb"
-    T.matchN "b*b*b*/**" "bb"
-
-    T.matchY "**" "/" ["//"] -- UGLY corner case
-    T.matchY "**/x" "/x" ["/"]
-    T.matchY "**" "x/" ["x/"]
-    let s = if isWindows then '/' else '\\'
-    T.matchY "**" "\\\\drive" [s:s:"drive"]
-    T.matchY "**" "C:\\drive" ["C:"++s:"drive"]
-    T.matchY "**" "C:drive" ["C:drive"]
-
-    -- We support ignoring '.' values in FilePath as they are inserted by @filepath@ a lot
-    -- T.matchY "./file" "file" []
-    T.matchN "/file" "file"
-    -- T.matchY "foo/./bar" "foo/bar" []
-    T.matchY "foo/./bar" "foo/./bar" []
-    -- T.matchY "foo/./bar" "foo/bar" []
 
 {-
 testWalk :: Switch -> IO ()

2f2f04f263847c8bc2f83ef35af5e89c3d31df7e
Neil Mitchell <ndmitchell@gmail.com>
2024-01-18 10:14:58 +0000

    Avoid use of head to satisfy GHC 9.8

--- src/System/FilePattern/Tree.hs
+++ src/System/FilePattern/Tree.hs
@@ -13,7 +13,7 @@ data Tree k v = Tree [v] [(k, Tree k v)]
 makeTree :: Ord k => [(v, [k])] -> Tree k v
 makeTree = f . sortOn snd
     where
-        f xs = Tree (map fst empty) [(fst $ head gs, f $ map snd gs) | gs <- groups]
+        f xs = Tree (map fst empty) [(fst g1, f $ map snd gs) | gs@(g1:_) <- groups]
             where
                 (empty, nonEmpty) = span (null . snd) xs
                 groups = groupOn fst [(x, (a,xs)) | (a,x:xs) <- nonEmpty]
